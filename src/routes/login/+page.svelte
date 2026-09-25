1d6b223b5e3af982ccc3650efd2a653d115793f6
Rogen <rogenbarrow@gmail.com>
2026-09-25 19:59:06 -0400

    Add login and signup page with Supabase auth

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    /** @type {import('./$types').PageProps} */
+    let { form } = $props();
+</script>
+
+<form method="POST" action="?/login">
+<label for="email">Email</label>
+<input type="email" name="email" id="email" value={form?.email ?? ''} required>
+<label for="password">Password</label>
+<input type="password" name="password" id="password" required>
+{#if form?.message }
+    <p class="text-red-600">Message: {form?.message}</p>
+{/if}
+
+<button>Login</button>
+<button formaction="?/signup">Signup</button>
+</form>
